8e6be2d94785a6cde4270fe507daad9f7069db05
amithasanamithasan <amit35-1773@diu.edu.bd>
2025-04-29 02:05:27 +0600

    props

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,14 +9,14 @@ import Lottie from 'lottie-react'
 
 const Hero = () => {
   return (
-    <section className='relative'>
+    <section className='relative bg-[#ffffff]'>
       <div className='max-w-7xl mx-auto  py-10'>
         <div className='flex flex-col md:flex-row items-center lg:h-[90vh] justify-between' >
             <div className='md:w-1/2 mb-8 md:mb-0 flex flex-col space-y-4 px-6 lg:px-0 lg:mt-0 mt-10'>
                <h1 className='lg:text-7xl text-4xl font-bold lg:leading-snug '>Hi There, <br />I am Amit Hasan <span className='text-red-400'> Faysal</span></h1>
                <p className='md:text-2xl text-xl mb-4'>Web Developer & Designer</p>
                <p className='mb-4'>I am a passionate web developer with expertise in React, Next.js, and modern web technologies. I love creating beautiful and functional website that solve real world problems.</p>
-               <button className='bg-black text-white px-3 py-2 w-max rounded-md'><a href="https://drive.google.com/file/d/1LBMHWpTgTGkvCTcHYiU8JQRCHb5-SRl8/view?usp=sharing" download target='_blank'><Lottie/> Download CV</a></button>
+               <button className='bg-[#391752] text-white px-3 py-2 w-max rounded-md'><a href="https://drive.google.com/file/d/1LBMHWpTgTGkvCTcHYiU8JQRCHb5-SRl8/view?usp=sharing" download target='_blank'><Lottie/> Download CV</a></button>
             </div>
             <div className='md:w-1/2 relative flex justify-center items-end'>
                  <img src={profilepic} alt="Rounded avatar"  className='lg:h-[90vh] h-96 rounded-full ' />
