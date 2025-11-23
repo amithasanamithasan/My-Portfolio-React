a887259b8b5e83d04c60d97e664ef0a5afa110ca
amithasanamithasan <amit35-1773@diu.edu.bd>
2025-11-23 13:17:14 +0600

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
 
-  ],
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: "./",
+  build: {
+    chunkSizeWarningLimit: 1000, // increase warning limit to 1MB
+    rollupOptions: {
+      output: {
+        // split node_modules into a vendor chunk
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
